e0591f7281e714c30d1c81afff0332d608c4db6c
Matt Frear <matt.frear@gmail.com>
2018-02-27 12:03:09 +0000

    initial version

---
+++ SlowNCrunch/Fixtures/MyAutoDataAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Ploeh.AutoFixture.Xunit2;
+using System;
+using System.Threading;
+
+namespace SlowNCrunch.Fixtures
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class MyAutoDataAttribute : AutoDataAttribute
+    {
+        public MyAutoDataAttribute()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+        }
+    }
+}
---
+++ SlowNCrunch/Tests/Test.cs
@@ -0,0 +1,17 @@
+﻿using Shouldly;
+using SlowNCrunch.Fixtures;
+using Xunit;
+
+namespace SlowNCrunch
+{
+    public class Test
+    {
+        // [Scenario]
+        [Fact]
+        [MyAutoData]
+        public void Test1()
+        {
+            true.ShouldBe(true);
+        }
+    }
+}
